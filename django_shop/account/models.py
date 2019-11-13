d365fa78a52539c5a88a3c81a2b096d4db262afc
jihoon <blue_jihoon@naver.com>
2019-11-13 17:23:51 +0900

    add frame of account view

--- django_shop/account/models.py
+++ django_shop/account/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    birth = models.DateField(blank=True, null=True)
+
+
+class Action(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='actions', db_index=True, on_delete=models.CASCADE)
+    verb = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    content_type = models.ForeignKey(ContentType, blank=True, null=True, related_name='content_obj',
+                                     on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField(null=True, blank=True, db_index=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
--- django_shop/account/views.py
+++ django_shop/account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 유저 등록
+def register_user(request):
+    pass
+
+# user 정보 변경
+def edit_user(request):
+    pass
+
