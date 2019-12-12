5edd21c4d1abec771fde78afab93ffe55641dd30
jihoon <blue_jihoon@naver.com>
2019-12-12 18:47:21 +0900

    add a comment

--- django_shop/shop/pattern_singleton.py
+++ django_shop/shop/pattern_singleton.py
@@ -1,6 +1,8 @@
 class Singleton:
     _instance = None
 
+    # Cart의 경우 입력된 매개변수(request)가 반영되어야 한다.
+    # request를 요청한 사용자마다 다른 request.session을 사용해야 한다.
     def __new__(cls, *args):
         # Cart의 객체인지 확인
         # 아래 조건문이 True 경우, 새로운 cls._instance 생성 후 반환, False일 경우 None 반환
